7b919f7fb30e83aa380f2508c0f8aa40860aa4dc
Augusto Moscardo <augustomsaluz@gmail.com>
2024-09-23 21:38:32 -0300

    Cofing layout & Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-br" className={`${inter.variable} antialiased`}>
-      <body>{children}</body>
+    <html lang="pt-br" className={`${inter.variable} `}>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
